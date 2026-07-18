f2285ee1bc760bb5762e098d38f02b1da85ad3ed
Pro203S <prosirea2501@gmail.com>
2026-07-18 10:31:19 +0900

    chore: add debug build launcher

---
+++ debug.bat
@@ -0,0 +1,3 @@
+dotnet build c:\Users\User\source\repos\Nocturne\Nocturne\Nocturne.csproj /property:GenerateFullPaths=true /p:Configuration=Debug /p:Platform=AnyCPU /consoleloggerparameters:NoSummary
+
+"C:\Program Files\WindowsApps\Microsoft.WindowsTerminal_1.24.11911.0_x64__8wekyb3d8bbwe\WindowsTerminal.exe"
