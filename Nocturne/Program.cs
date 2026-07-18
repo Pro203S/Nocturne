a78f2a3b2a255e5247357dc78c3d7d619becd5b8
Pro203S <prosirea2501@gmail.com>
2026-07-18 10:31:09 +0900

    feat: implement interactive shell loop

--- Nocturne/Program.cs
+++ Nocturne/Program.cs
@@ -1,11 +1,68 @@
-﻿namespace Nocturne
+﻿using System.Text;
+using Nocturne.Utils;
+
+namespace Nocturne
 {
     public class Program
     {
+        static string _path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
         static void Main(string[] args)
         {
-            string? initialPath = args.ElementAtOrDefault(0);
-            Console.WriteLine(initialPath);
+            _path = args.ElementAtOrDefault(0) ?? _path;
+
+            Console.Clear();
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Profile.Load();
+
+            if (Convert.ToBoolean(Environment.GetEnvironmentVariable("NOCTURNE_HELP_MSG")))
+            {
+                Console.Write(
+                    "Welcome to {0} 🌙\n\nType {1} to experience special.\n\n",
+                    Colors.Bold(Colors.BrightWhite("Nocturne shell")),
+                    Colors.Bold(Colors.BrightYellow("/help"))
+                );
+            }
+
+            for (; ; )
+            {
+                string? input = GetInput();
+                if (string.IsNullOrEmpty(input)) continue;
+
+                if (input.Trim() == "exit")
+                {
+                    Environment.Exit(0);
+                    return;
+                }
+
+                if (input[0] != '/')
+                {
+                    Profile.Execute(input);
+                    continue;
+                }
+            }
+        }
+
+        static string? GetInput()
+        {
+            string userName = Environment.UserName;
+            string computerName = Environment.MachineName;
+
+            Console.Write("{0}{1}{2}{3}{4}{5}{6}{4}\n",
+                Colors.Blue("┌─["),
+                Colors.Bold(Colors.BrightGreen(userName)),
+                Colors.BrightBlue("@"),
+                Colors.Bold(Colors.BrightBlue(computerName)),
+                Colors.Blue("]"),
+                Colors.Blue("──["),
+                Colors.Bold(Colors.BrightWhite(_path))
+            );
+            Console.Write("{0}{1}{2} ",
+                Colors.Blue("└─["),
+                Colors.Bold(Colors.BrightYellow("$")),
+                Colors.Blue("]")
+            );
+            return Console.ReadLine();
         }
     }
 }
